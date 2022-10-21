a1df50c5f89c7d9d8e6cb4e431fbc34c8ce854f0
Ashutosh <ashutoshshukl99@gmail.com>
2022-10-21 14:24:55 +0530

    Demo commited done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/", (req, res)=>{
-  res.json("Server Started Successfully")
+  res.json("Server Started Successfully Done")
 })
 
 app.use('/api/auth', require('./routes/auth'))
